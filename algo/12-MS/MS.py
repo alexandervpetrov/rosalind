abdfb05e752a4a6d5501658e50da7ba569ac51ff
Alexander Petrov <alexandervpetrov@gmail.com>
2016-02-09 02:02:26 +0200

    MS problem

---
+++ algo/12-MS/MS.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import sys
+ifs = sys.stdin
+ofs = sys.stdout
+
+
+def numbers_from_line(d=' '):
+    return [int(s) for s in ifs.readline().strip().split(d)
+            if len(s.strip()) > 0]
+
+
+n = int(ifs.readline())
+A = numbers_from_line()
+
+
+def merge_sorted(A, B):
+    n = len(A)
+    m = len(B)
+    C = []
+    i = 0
+    j = 0
+    while i < n and j < m:
+        if A[i] <= B[j]:
+            C.append(A[i])
+            i += 1
+        else:
+            C.append(B[j])
+            j += 1
+    if i < n:
+        C.extend(A[i:])
+    if j < m:
+        C.extend(B[j:])
+    return C
+
+
+def sorted_by_merging(A):
+    n = len(A)
+    if n == 1:
+        return A
+    elif n == 2:
+        return [min(A[0], A[1]), max(A[0], A[1])]
+    else:
+        A1 = sorted_by_merging(A[n/2:])
+        A2 = sorted_by_merging(A[:n/2])
+        return merge_sorted(A1, A2)
+
+
+C = sorted_by_merging(A)
+
+ofs.write('%s\n' % ' '.join(map(str, C)))
