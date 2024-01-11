345a32ac7731793c754fd21c688d3ef327ac94af
Oleksandr Petrov <alexandervpetrov@gmail.com>
2024-01-11 10:59:29 +0200

    Update partition algorithms

--- algo/13-PAR/PAR.py
+++ algo/13-PAR/PAR.py
@@ -14,47 +14,69 @@ def numbers_from_line(d=' '):
     ]
 
 
+def identity(x):
+    return x
+
+
 def swap(A, i, j):
     A[i], A[j] = A[j], A[i]
 
 
-def partition(A, beg, end, pivot_pos):
+def partition(A, beg, end, pivot_pos, key_fn=identity):
+
+    assert beg <= pivot_pos <= end
 
     pivot = A[pivot_pos]
+    pivot_key = key_fn(pivot)
+
     swap(A, beg, pivot_pos)
 
-    l = beg
+    l = beg + 1
     r = end
     while l <= r:
-        if A[l] <= pivot:
-            l += 1
-        elif A[r] > pivot:
+        if key_fn(A[r]) > pivot_key:
             r -= 1
+        elif key_fn(A[l]) <= pivot_key:
+            l += 1
         else:
+            # here we have: A[l] > pivot and A[r] <= pivot
             swap(A, l, r)
-            l += 1
-            r -= 1
+            # indices will be updated on next iterations
+            # inside if-braches above
+    l -= 1
+    r += 1
+    assert l + 1 == r
 
-    swap(A, beg, l-1)
+    swap(A, beg, l)
+
+    # A[i] <= pivot for all i in [beg; l]
+    # A[l] == pivot
+    # A[i] > pivot for all i in [l+1; end]
+    verify(A, beg, end, pivot, l, key_fn=key_fn)
+
+    return l
 
 
-def verify(A, pivot):
-    p = len(A) - 1
-    while p >= 0 and A[p] > pivot:
-        p -= 1
-    assert p >= 0
-    assert A[p] == pivot
-    assert all(A[i] <= pivot for i in range(p + 1))
+def verify(A, beg, end, pivot, l, key_fn=identity):
+    assert beg <= l <= end
+    pivot_key = key_fn(pivot)
+    assert all(
+        key_fn(A[i]) <= pivot_key
+        for i in range(beg, l)
+    )
+    assert A[l] == pivot
+    assert all(
+        key_fn(A[i]) > pivot_key
+        for i in range(l+1, end+1)
+    )
 
 
 n = int(ifs.readline())
 A = numbers_from_line()
 
 pivot_pos = 0
-pivot = A[pivot_pos]
 
 partition(A, 0, len(A)-1, pivot_pos)
-verify(A, pivot)
 
 ofs.write(' '.join(map(str, A)))
 ofs.write('\n')
--- algo/20-PAR3/PAR3.py
+++ algo/20-PAR3/PAR3.py
@@ -7,34 +7,87 @@ ofs = sys.stdout
 
 
 def numbers_from_line(d=' '):
-    return [int(s) for s in ifs.readline().strip().split(d)
-            if len(s.strip()) > 0]
+    return [
+        int(s)
+        for s in ifs.readline().strip().split(d)
+        if len(s.strip()) > 0
+    ]
 
 
-def partition(A, p):
-    n = len(A)
-    if n < 2:
-        return
-    beg = 0
-    end = 0
-    for i in range(n):
-        a = A[i]
-        if a > p:
-            continue
-        if a == p:
-            A[end], A[i] = A[i], A[end]
-            end += 1
+def identity(x):
+    return x
+
+
+def swap(A, i, j):
+    A[i], A[j] = A[j], A[i]
+
+
+def partition(A, beg, end, pivot_pos, key_fn=identity):
+
+    assert beg <= pivot_pos <= end
+
+    pivot = A[pivot_pos]
+    pivot_key = key_fn(pivot)
+
+    swap(A, beg, pivot_pos)
+
+    l = beg + 1
+    ml = beg + 1
+    mr = end
+    r = end
+    while ml <= mr:
+        if key_fn(A[mr]) == pivot_key:
+            mr -= 1
+        elif key_fn(A[mr]) > pivot_key:
+            swap(A, mr, r)
+            mr -= 1
+            r -= 1
+        elif key_fn(A[ml]) == pivot_key:
+            ml += 1
+        elif key_fn(A[ml]) < pivot_key:
+            swap(A, l, ml)
+            l += 1
+            ml += 1
         else:
-            A[beg], A[i] = A[i], A[beg]
-            beg += 1
-            A[end], A[i] = A[i], A[end]
-            end += 1
-    return A
+            # here we have: A[ml] > pivot and A[mr] < pivot
+            swap(A, ml, mr)
+            # other swaps and indices updates will be on next iterations
+            # inside if-braches above
+
+    l -= 1
+    swap(A, beg, l)
+
+    # A[i] < pivot for all i in [beg; l)
+    # A[i] == pivot for all i in [l; r]
+    # A[i] > pivot for all i in (r; end]
+    verify(A, beg, end, pivot, l, r, key_fn=key_fn)
+
+    return l, r
+
+
+def verify(A, beg, end, pivot, l, r, key_fn=identity):
+    assert beg <= l <= r <= end
+    pivot_key = key_fn(pivot)
+    assert all(
+        key_fn(A[i]) < pivot_key
+        for i in range(beg, l)
+    )
+    assert all(
+        key_fn(A[i]) == pivot_key
+        for i in range(l, r+1)
+    )
+    assert all(
+        key_fn(A[i]) > pivot_key
+        for i in range(r+1, end+1)
+    )
 
 
 n = int(ifs.readline())
 A = numbers_from_line()
 
-partition(A, A[0])
+pivot_pos = 0
 
-ofs.write('%s\n' % ' '.join(map(str, A)))
+partition(A, 0, len(A)-1, pivot_pos)
+
+ofs.write(' '.join(map(str, A)))
+ofs.write('\n')
